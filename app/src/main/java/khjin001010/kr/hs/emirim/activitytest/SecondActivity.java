ce6616509e8713c2252eae0d71c05f1c9ee61725
khjin001010 <khjin001010@naver.com>
2017-04-13 20:13:59 +0900

    #3 finish

--- app/src/main/java/khjin001010/kr/hs/emirim/activitytest/SecondActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/activitytest/SecondActivity.java
@@ -2,12 +2,22 @@ package khjin001010.kr.hs.emirim.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button but_second = (Button)findViewById(R.id.but_second);
+        but_second.setOnClickListener(butSecondHandler);
+
     }
+    View.OnClickListener butSecondHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
 }
